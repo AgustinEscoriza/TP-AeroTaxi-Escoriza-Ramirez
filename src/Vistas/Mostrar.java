5e92550c3d8c03bd49668c397032b2f749449ad6
RMFacundo <ramirez.facu@hotmail.com>
2020-06-06 12:44:48 -0300

    agregados algunos metodos en clase mostrar

--- src/Vistas/Mostrar.java
+++ src/Vistas/Mostrar.java
@@ -1,4 +1,39 @@
 package Vistas;
 
+import LogicaDeNegocio.Aerolinea;
+import Modelo.Vuelo;
+import Modelo.Avion;
+import Modelo.Usuario;
+import java.time.LocalDate;
+
 public class Mostrar {
+    public Aerolinea aerolinea;
+
+    public Mostrar(){
+        this.aerolinea = new Aerolinea();
+    }
+
+    public Mostrar(Aerolinea aerolinea) {
+        this.aerolinea = aerolinea;
+    }
+
+    public void mostrarVuelosPorFecha(LocalDate fecha){
+        for(aerolinea.vuelos: Vuelo vuelo){
+            if(vuelo.getFecha()==fecha){
+                System.out.println(vuelo.toString());
+            }
+        }
+    }
+
+    public void mostrarVuelos(){
+        for(aerolinea.vuelos : Vuelo vuelo){
+            System.out.println(vuelo.toString());
+        }
+    }
+
+    public void mostrarUsuarios(){
+        for(aerolinea.usuarios : Usuario usuario){
+            System.out.println(usuario.toString());
+        }
+    }
 }
