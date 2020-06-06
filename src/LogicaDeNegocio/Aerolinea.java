0d45800487e6db31210791a10aa69f1bd574a7f6
RMFacundo <ramirez.facu@hotmail.com>
2020-06-06 12:13:43 -0300

    actualizada clase aerolinea

--- src/LogicaDeNegocio/Aerolinea.java
+++ src/LogicaDeNegocio/Aerolinea.java
@@ -1,5 +1,27 @@
 package LogicaDeNegocio;
 
+import Modelo.Avion;
+import Modelo.Vuelo;
+import java.util.ArrayList;
+
 public class Aerolinea {
+    public ArrayList<Avion> aviones;
+    public ArrayList<Vuelo> vuelos;
+
+    public Aerolinea(){
+        this.aviones = new ArrayList<Avion>();
+        this.vuelos = new ArrayList<Vuelo>();
+    }
+
+    public void agregarAvion(Avion avion){
+        aviones.add(avion);
+    }
+
+    public void agregarVuelo(Vuelo vuelo){
+        vuelos.add(vuelo);
+    }
+
+
+
 
 }
