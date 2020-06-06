f45957e41bb198f5bac09c5003a975a7d21d6d5d
AgustinEscoriza <51381601+AgustinEscoriza@users.noreply.github.com>
2020-06-05 23:31:57 -0300

    agregada clase vuelo

--- src/Modelo/Vuelo.java
+++ src/Modelo/Vuelo.java
@@ -1,4 +1,53 @@
 package Modelo;
 
+import java.time.LocalDate;
+import Enums.Ciudad;
+
 public class Vuelo {
+
+    private Avion avion;
+    private int cantidadAcompañantes;
+    private Ciudad origen;
+    private Ciudad destino;
+    private LocalDate fecha;
+    private float costo;
+    private boolean confirmacion;
+
+    public Vuelo(Avion avion, int cantidadAcompañantes, Ciudad origen, Ciudad destino, LocalDate fecha, float costo, boolean confirmacion) {
+        this.avion = avion;
+        this.cantidadAcompañantes = cantidadAcompañantes;
+        this.origen = origen;
+        this.destino = destino;
+        this.fecha = fecha;
+        this.costo = costo;
+        this.confirmacion = confirmacion;
+    }
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public int getCantidadAcompañantes() {
+        return cantidadAcompañantes;
+    }
+
+    public Ciudad getOrigen() {
+        return origen;
+    }
+
+    public Ciudad getDestino() {
+        return destino;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public boolean isConfirmacion() {
+        return confirmacion;
+    }
 }
