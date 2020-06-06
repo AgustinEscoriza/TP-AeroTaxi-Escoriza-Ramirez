1233791089537b9f7b530115e06c2d7e013b39e2
RMFacundo <ramirez.facu@hotmail.com>
2020-06-06 12:37:38 -0300

    creada clase usuario

---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private int edad;
+
+    public Usuario(String nombre, String apellido, String dni, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
