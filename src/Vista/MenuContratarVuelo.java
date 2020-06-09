1ab7ecfc123693f2b843a8b358b1945da814dc2f
RMFacundo <ramirez.facu@hotmail.com>
2020-06-09 20:23:30 -0300

    actualizado metodo menuContratarVuelo

--- src/Vista/MenuContratarVuelo.java
+++ src/Vista/MenuContratarVuelo.java
@@ -2,24 +2,49 @@ package Vista;
 
 import java.util.Scanner;
 import Modelo.Vuelo;
+import Modelo.Avion;
+import Enums.Ciudad;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class MenuContratarVuelo {
-
-    private Scanner sn;
-    private boolean salir;
-    private int opcion;
+public class MenuContratarVuelo extends MenuPrincipal{
 
     public MenuContratarVuelo() {
-        sn = new Scanner(System.in);
-        salir=false;
-        opcion=0;
+        super();
     }
 
-    public Vuelo menu(){
-        while(!salir){
-
+    public Vuelo menuContratarVuelo() {
+        Vuelo vuelo=null;
+        Avion avion=null;
+        String fechaString;
+        Ciudad destino=null;
+        Ciudad origen=null;
+        float costoTotal;
+        int distanciaKms;
+        String confirmacion = null;
+        int cantidadAcompanantes;
+        while (!salir) {
+            System.out.println("Ingrese fecha");
+            fechaString = sn.next();
+            LocalDate fechaLocalDate = LocalDate.parse(fechaString);
+            //SELECCION ORIGEN Y DESTINO//
+            System.out.println("Ingrese cantidad de acompanantes");
+            cantidadAcompanantes = sn.nextInt();
+            //SELECCION DE AVION//
+            while (confirmacion != "S" || confirmacion != "N") {
+                System.out.println("Confirmar? S/N");
+                confirmacion = sn.next();
+                if (confirmacion == "S") {
+                    vuelo = new Vuelo(avion, origen, destino, cantidadAcompanantes, fechaLocalDate);
+                } else if (confirmacion == "N") {
+                    vuelo = null;
+                } else {
+                    System.out.println("Ingrese S o N");
+                }
+            }
+            salir=salirAMenuAnterior();
         }
-
+        return vuelo;
     }
 
 
