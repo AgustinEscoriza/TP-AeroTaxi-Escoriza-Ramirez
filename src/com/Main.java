bcc362e7f96f32d240b09080dcc64f3c7a4d5351
RMFacundo <ramirez.facu@hotmail.com>
2020-06-12 11:00:26 -0300

    actualizado main

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,6 +1,7 @@
 package com;
 
 import LogicaDeNegocio.Aerolinea;
+import Vista.MenuLogin;
 import Vista.MenuPrincipal;
 
 public class Main {
@@ -8,7 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Aerolinea aerolinea = new Aerolinea();
 
+        MenuLogin menuLogin = new MenuLogin(aerolinea);
         MenuPrincipal menuPrincipal = new MenuPrincipal(aerolinea);
-        menuPrincipal.menuPrincipal();
+
+        menuPrincipal.menuPrincipal(aerolinea, menuLogin.menuLogin(aerolinea));
     }
 }
