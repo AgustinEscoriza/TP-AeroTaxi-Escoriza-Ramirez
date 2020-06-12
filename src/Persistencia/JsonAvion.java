202910ba56c7a4b502bd09b702a3f14f82140cae
AgustinEscoriza <51381601+AgustinEscoriza@users.noreply.github.com>
2020-06-12 05:13:10 -0300

    modificadas vistas

---
+++ src/Persistencia/JsonAvion.java
@@ -0,0 +1,13 @@
+package Persistencia;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class EscribirJsonAvion {
+
+    File archivo = new File("datos.json");
+    ObjectMapper mapper = new ObjectMapper();
+
+
+}
