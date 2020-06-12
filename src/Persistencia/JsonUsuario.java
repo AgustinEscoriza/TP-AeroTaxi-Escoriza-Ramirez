44b912c8a675cc85bda444072d09f269fa651635
AgustinEscoriza <51381601+AgustinEscoriza@users.noreply.github.com>
2020-06-12 07:43:10 -0300

    actualizadas clases Json

--- src/Persistencia/JsonUsuario.java
+++ src/Persistencia/JsonUsuario.java
@@ -1,6 +1,7 @@
 package Persistencia;
 
-import Modelo.Avion;
+
+import Modelo.Usuario;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,5 +14,37 @@ public class JsonUsuario {
 
     private static ObjectMapper mapper = new ObjectMapper();
     private static String archivo = "usuario.json";
+
+    public static void aJson(ArrayList<Usuario> usuarios){
+        try {
+            File json = new File(archivo);
+            mapper.writeValue(json,);
+        } catch (JsonMappingException e){
+            e.printStackTrace();
+        } catch (JsonGenerationException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static ArrayList<Usuario> aJava(){
+        Usuario[] arrayUsuario = new Usuario[0];
+        ArrayList<Usuario> usuarios = null;
+        try{
+
+            File json = new File(archivo);
+            arrayUsuario = mapper.readValue(json,Usuario[].class);
+            usuarios = usuarioToArrayList(arrayUsuario);
+
+        } catch (JsonMappingException e){
+            e.printStackTrace();
+        } catch (JsonGenerationException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return usuarios;
+    }
 
 }
--- src/Persistencia/JsonVuelo.java
+++ src/Persistencia/JsonVuelo.java
@@ -1,6 +1,6 @@
 package Persistencia;
 
-import Modelo.Avion;
+import Modelo.Vuelo;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,5 +13,37 @@ public class JsonVuelo {
 
     private static ObjectMapper mapper = new ObjectMapper();
     private static String archivo = "vuelo.json";
+
+    public static void aJson(ArrayList<Vuelo> vuelos){
+        try {
+            File json = new File(archivo);
+            mapper.writeValue(json,vuelos);
+        } catch (JsonMappingException e){
+            e.printStackTrace();
+        } catch (JsonGenerationException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static ArrayList<Vuelo> aJava(){
+        Vuelo[] arrayVuelos = new Vuelo[0];
+        ArrayList<Vuelo> vuelos = null;
+        try{
+
+            File json = new File(archivo);
+            arrayVuelos = mapper.readValue(json,Vuelo[].class);
+            vuelos = vueloToArrayList(arrayVuelos);
+
+        } catch (JsonMappingException e){
+            e.printStackTrace();
+        } catch (JsonGenerationException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return vuelos;
+    }
 
 }
